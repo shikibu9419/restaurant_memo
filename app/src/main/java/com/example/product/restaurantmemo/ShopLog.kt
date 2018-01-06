ca5b59970a0ca9f35421b52cf4a20fb0b482c85d
Shikibu <shikibu9419@gmail.com>
2018-01-07 01:13:18 +0900

    [add] CoordinatorLayout用パッケージの追加

--- app/src/main/java/com/example/product/restaurantmemo/ShopLog.kt
+++ app/src/main/java/com/example/product/restaurantmemo/ShopLog.kt
@@ -7,11 +7,11 @@ import io.realm.annotations.RealmClass
 
 @RealmClass
 open class ShopLog(
-        @PrimaryKey open var id: Long = 0,
-        open var placeId: String = "",
-        open var comment: String? = null,
-        open var numStars: Float = 0f,
-        @Ignore open var aveNumStars: Float = 0f,
-        @Ignore open var numVisits: Int = 0,
-        @Ignore open var latestLog: String = ""
+        @PrimaryKey var id: Long = 0,
+        var placeId: String = "",
+        var comment: String? = null,
+        var numStars: Float = 0f,
+        @Ignore var aveNumStars: Float = 0f,
+        @Ignore var numVisits: Int = 0,
+        @Ignore var latestLog: String = ""
 ): RealmObject(){}

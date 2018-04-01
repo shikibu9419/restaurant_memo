99239f399bbf77e551f5c23f90931843ca0773d9
Shikibu <shikibu9419@gmail.com>
2018-04-01 23:42:38 +0900

    Update Android Studio

--- app/src/main/java/com/example/product/restaurantmemo/ShopLog.kt
+++ app/src/main/java/com/example/product/restaurantmemo/ShopLog.kt
@@ -7,9 +7,9 @@ import java.util.*
 
 @RealmClass
 open class ShopLog(
-        @PrimaryKey var id: Long = 0,
-        var placeId: String = "",
-        var comment: String = "",
-        var starRating: Float = 0f,
-        var logDate: Date = Date()
+    @PrimaryKey var id: Long = 0,
+    var placeId: String = "",
+    var comment: String = "",
+    var starRating: Float = 0f,
+    var logDate: Date = Date()
 ): RealmObject(){}
